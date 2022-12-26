dd5caaf718c2bb551632e14d4e63470a0babe848
Krunal Thakkar <121278477+Krunal-Thakkar@users.noreply.github.com>
2022-12-26 14:37:26 +0530

    Update the version

--- versions.tf
+++ versions.tf
@@ -2,8 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.0"
     }
+
+    
 
     random = {
       source  = "hashicorp/random"
